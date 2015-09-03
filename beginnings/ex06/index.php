4d4ba90d43acd9ba310951f018cc0b1ad88f0909
Jibin Joseph Sonny <jibinsonny@gmail.com>
2015-09-03 16:05:45 +0530

    Added the solution to beginnings/ex06.

--- beginnings/ex06/index.php
+++ beginnings/ex06/index.php
@@ -1,2 +1,17 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $around = "around";
+        echo 'What goes ' . $around . ' comes ' . $around . '.';
+        
+      ?>
+    </p>
+  </body>
+</html>
+
+
